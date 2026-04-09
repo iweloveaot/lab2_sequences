f716851e3c6832803b9c6ef815b01806db2037c1
vinny <viknikgon@yandex.ru>
2026-04-09 20:41:07 +0300

    little changes in bit

--- bit_sequence.h
+++ bit_sequence.h
@@ -1,79 +1,73 @@
 #ifndef _BIT_SEQUENCE_H_
 #define _BIT_SEQUENCE_H_
 
-#include "sequence.h"
-#include "dynamic_array.h"
+#include "mutable_array_sequence.h"
 #include "exceptions.h"
 
-// ===== Класс Bit =====
 class Bit {
 private:
     bool value;
-    
+
 public:
     Bit(bool val = false) : value(val) {}
-    
+
     Bit& operator=(bool val) {
         value = val;
         return *this;
     }
-    
-    operator bool() const { 
-        return value; 
-    }
-    
-    Bit operator~() const { 
-        return Bit(!value); 
-    }
-    
-    Bit operator&(const Bit& other) const {
-        return Bit(value && other.value); 
+
+    operator bool() const {
+        return value;
     }
 
-    Bit operator|(const Bit& other) const { 
-        return Bit(value || other.value); 
+    Bit operator~() const {
+        return Bit(!value);
+    }
+
+    Bit operator&(const Bit& other) const {
+        return Bit(value && other.value);
+    }
+
+    Bit operator|(const Bit& other) const {
+        return Bit(value || other.value);
     }
 
     Bit operator^(const Bit& other) const {
-        return Bit(value != other.value); 
+        return Bit(value != other.value);
     }
-    
+
     Bit& operator&=(const Bit& other) {
-        value = value && other.value; 
-        return *this; 
+        value = value && other.value;
+        return *this;
     }
 
     Bit& operator|=(const Bit& other) {
-        value = value || other.value; 
-        return *this; 
+        value = value || other.value;
+        return *this;
     }
 
-    Bit& operator^=(const Bit& other) { 
-        value = value != other.value; 
-        return *this; 
+    Bit& operator^=(const Bit& other) {
+        value = value != other.value;
+        return *this;
     }
-    
+
     bool operator==(const Bit& other) const {
-        return value == other.value; 
+        return value == other.value;
     }
 
     bool operator!=(const Bit& other) const {
-        return value != other.value; 
+        return value != other.value;
     }
 };
 
-
-class BitSequence : public Sequence<Bit> {
+class BitSequence : public MutableArraySequence<Bit> {
 private:
-    DynamicArray<Bit> bits;
-    
-
-    BitSequence* applyBitwise(const BitSequence& other, 
-                            Bit (Bit::*op)(const Bit&) const) const {
+    BitSequence* applyBitwise(const BitSequence& other,
+                              Bit (Bit::*op)(const Bit&) const) const {
         if (GetLength() != other.GetLength()) {
             throw InvalidArgumentException("BitSequence lengths must match for bitwise operations");
         }
-        
+
         BitSequence* result = new BitSequence();
         for (int i = 0; i < GetLength(); ++i) {
             result->AppendImplict((Get(i).*op)(other.Get(i)));
@@ -81,161 +75,62 @@ private:
         return result;
     }
 
-protected:
-    
-    virtual Sequence<Bit>* Instance() override {
-        return this;
-    }
-    
-    virtual Sequence<Bit>* AppendImplict(const Bit& item) override {
-        int size = bits.GetSize();
-        bits.Resize(size+1);
-        bits.Set(size, item);
-        return this;
-    }
-    
-    virtual Sequence<Bit>* PrependImplict(const Bit& item) override {
-        DynamicArray<Bit> newArr(bits.GetSize() + 1);
-        newArr.Set(0, item);
-        for (int i = 0; i < bits.GetSize(); ++i) {
-            newArr.Set(i + 1, bits.Get(i));
-        }
-        bits = newArr;
-        return this;
-    }
-    
-    virtual Sequence<Bit>* InsertAtImplict(const Bit& item, int index) override {
-        if (index < 0 || index > bits.GetSize())
-            throw IndexOutOfRangeException("Index out of range in BitSequence::InsertAt");
-        
-        if (index == bits.GetSize()) {
-            return AppendImplict(item);
-        }
-        if (index == 0) {
-            return PrependImplict(item);
-        }
-        
-        DynamicArray<Bit> newArr(bits.GetSize() + 1);
-        for (int i = 0; i < index; ++i)
-            newArr.Set(i, bits.Get(i));
-        newArr.Set(index, item);
-        for (int i = index; i < bits.GetSize(); ++i)
-            newArr.Set(i + 1, bits.Get(i));
-        bits = newArr;
-        return this;
-    }
-    
-    virtual Sequence<Bit>* ConcatImplict(const Sequence<Bit>* other) override {
-        if (!other) throw InvalidArgumentException("Null sequence in Concat");
-        for (int i = 0; i < other->GetLength(); ++i) {
-            AppendImplict(other->Get(i));
-        }
-        return this;
-    }
-    
 public:
-    BitSequence() : bits() {}
-    
-    BitSequence(const bool* items, int count) : bits() {
+    BitSequence() : MutableArraySequence<Bit>() {}
+
+    BitSequence(const bool* items, int count) : MutableArraySequence<Bit>() {
         if (count < 0) throw InvalidArgumentException("Negative count");
         for (int i = 0; i < count; ++i) {
             AppendImplict(Bit(items[i]));
         }
     }
-    BitSequence(const Bit* items, int count) : bits(items, count) {}
-    explicit BitSequence(int size) : bits(size) {}
-    BitSequence(const DynamicArray<Bit>& arr) : bits(arr) {}
-    BitSequence(const BitSequence& other) : bits(other.bits) {}
+
+    BitSequence(const Bit* items, int count) : MutableArraySequence<Bit>(items, count) {}
+
+    explicit BitSequence(int size) : MutableArraySequence<Bit>(size) {}
+
+    BitSequence(const DynamicArray<Bit>& arr) : MutableArraySequence<Bit>(arr) {}
+
+    BitSequence(const BitSequence& other) : MutableArraySequence<Bit>(other) {}
+
     ~BitSequence() override {};
-    
-    const Bit& GetFirst() const override {
-        if (bits.GetSize() == 0)
-            throw EmptyContainerException("BitSequence is empty");
-        return bits.Get(0);
-    }
-    
-    const Bit& GetLast() const override {
-        if (bits.GetSize() == 0)
-            throw EmptyContainerException("BitSequence is empty");
-        return bits.Get(bits.GetSize() - 1);
-    }
-    
-    const Bit& Get(int index) const override {
-        return bits.Get(index);
-    }
-    
-    Sequence<Bit>* GetSubsequence(int startIndex, int endIndex) const override {
-        if (startIndex < 0 || endIndex >= bits.GetSize() || startIndex > endIndex)
+
+    // ===== Переопределение GetSubsequence для возврата правильного типа =====
+    BitSequence* GetSubsequence(int startIndex, int endIndex) const {
+        if (startIndex < 0 || endIndex >= GetLength() || startIndex > endIndex)
             throw IndexOutOfRangeException("Invalid subsequence indexes");
-        DynamicArray<Bit> tmp_array;
+        
+        DynamicArray<Bit> tmp_array(endIndex - startIndex + 1);
         int tmp_ind = 0;
         for (int i = startIndex; i <= endIndex; i++) {
-            tmp_array.Set(tmp_ind, bits.Get(i));
+            tmp_array.Set(tmp_ind, this->Get(i));
             tmp_ind++;
         }
         return new BitSequence(tmp_array);
     }
-    
-    int GetLength() const override {
-        return bits.GetSize();
-    }
-    
+
     BitSequence* operator~() const {
         BitSequence* result = new BitSequence();
-        for (int i = 0; i < bits.GetSize(); ++i) {
-            result->AppendImplict(~bits.Get(i));
+        for (int i = 0; i < GetLength(); ++i) {
+            result->AppendImplict(~this->Get(i));
         }
         return result;
     }
-    
+
     BitSequence* operator&(const BitSequence& other) const {
         return applyBitwise(other, &Bit::operator&);
     }
-    
+
     BitSequence* operator|(const BitSequence& other) const {
         return applyBitwise(other, &Bit::operator|);
     }
-    
+
     BitSequence* operator^(const BitSequence& other) const {
         return applyBitwise(other, &Bit::operator^);
     }
-    
-    
-    BitSequence& operator&=(const BitSequence& other) {
-        if (GetLength() != other.GetLength())
-            throw InvalidArgumentException("BitSequence lengths must match for &= operator");
-        for (int i = 0; i < bits.GetSize(); ++i) {
-            bits.Set(i, bits.Get(i) & other.Get(i));
-        }
-        return *this;
-    }
-    
-    BitSequence& operator|=(const BitSequence& other) {
-        if (GetLength() != other.GetLength())
-            throw InvalidArgumentException("BitSequence lengths must match for |= operator");
-        for (int i = 0; i < bits.GetSize(); ++i) {
-            bits.Set(i, bits.Get(i) | other.Get(i));
-        }
-        return *this;
-    }
-    
-    BitSequence& operator^=(const BitSequence& other) {
-        if (GetLength() != other.GetLength())
-            throw InvalidArgumentException("BitSequence lengths must match for ^= operator");
-        for (int i = 0; i < bits.GetSize(); ++i) {
-            bits.Set(i, bits.Get(i) ^ other.Get(i));
-        }
-        return *this;
-    }
-    
-    void SetBit(int index, bool value) {
-        if (index < 0 || index >= bits.GetSize())
-            throw IndexOutOfRangeException("Index out of range in BitSequence::SetBit");
-        bits.Set(index, Bit(value));
-    }
-    
+
     bool GetBit(int index) const {
-        return bool(Get(index));
+        return bool(this->Get(index));
     }
 };
 
--- main.cpp
+++ main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include "mutable_list_sequence.h"
+#include "bit_sequence.h"
 #include "option.h"
 
 bool isNotEven(const int &el) {
@@ -12,9 +12,23 @@ int half(const int &el) {
 
 int main() {
 
-    int items[] = {1, 3, 5, 7};
-    MutableListSequence<int> arr1 = MutableListSequence<int>(items, 4);
-    Option<int> opt = arr1.TryGetFirst(*isNotEven);
-    if (opt.HasValue()) std::cout << opt.GetValue();
+    bool items[] = {1, 0, 0, 0, 1};
+    bool items2[] = {1, 1, 0, 0, 1};
+    
+    BitSequence seq1 = BitSequence(items, 5);
+    BitSequence seq2 = BitSequence(items2, 5);
+    std::cout << seq2.GetBit(1) << std::endl;
+    BitSequence *seq1n = ~seq1;
+    BitSequence *seq1a = seq1&seq2;
+    BitSequence *seq1o = seq1|seq2;
+    BitSequence *seq1xo = seq1^seq2;
 
+    for (int i=0; i<5; i++) {
+        std::cout << seq1n->GetBit(i) << " " << seq1a->GetBit(i) << " " << seq1o->GetBit(i) << " " << seq1xo->GetBit(i) << std::endl;
+    }
+
+    delete seq1n;
+    delete seq1a;
+    delete seq1o;
+    delete seq1xo;
 }    
